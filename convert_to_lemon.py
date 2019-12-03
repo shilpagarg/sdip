8c490cde07047de381f5da647c4dbe0b3053d063
Shilpa Garg <shilpa.garg2k7@gmail.com>
2019-12-03 13:16:51 +0000

    fix failed assertion caused by duplicate lemon ids

--- convert_to_lemon.py
+++ convert_to_lemon.py
@@ -8,18 +8,27 @@ def readGFA(gfaFile):
         if line[0] == 'S':
             fields = line.split('\t')
             name = fields[1]
-            id = name.split("/")[1]
-            nodes.add(id)
+            parts = name.split("/")
+            run_time = parts[0]
+            zmw_hole_nr = parts[1]
+            lemon_id = zmw_hole_nr[-4:] + run_time[-4:]
+            nodes.add(lemon_id)
         elif line[0] == 'L':
             fields = line.split('\t')
             node1 = fields[1]
-            id1 = node1.split("/")[1]
+            node1_parts = node1.split("/")
+            run_time1 = node1_parts[0]
+            zmw_hole_nr1 = node1_parts[1]
             node1dir = fields[2]
-            id1_full = id1 + "1" if node1dir == "+" else id1 + "0"
+            id1_full = zmw_hole_nr1[-4:] + run_time1[-4:] + ("1" if node1dir == "+" else "0")
+
             node2 = fields[3]
-            id2 = node2.split("/")[1]
+            node2_parts = node2.split("/")
+            run_time2 = node2_parts[0]
+            zmw_hole_nr2 = node2_parts[1]
             node2dir = fields[4]
-            id2_full = id2 + "1" if node2dir == "+" else id2 + "0"
+            id2_full = zmw_hole_nr2[-4:] + run_time2[-4:] + ("1" if node2dir == "+" else "0")
+
             ovlp = int(fields[5][:-1])
             edges.append((id1_full, id2_full))
     nodes_directed = set()
